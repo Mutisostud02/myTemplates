e2416012bfaa41aa40d3658bfbba3283437dc953
Mutisostud02 <mutiso8395@gmail.com>
2024-01-09 12:28:04 +0300

    ...

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-import { li } from './home';
-import './style.css';
-// import {
-//   bodySettings,
-//   navSettings,
-//   mainUlSettings,
-//   menuListSettings,
-//   dropDownMenuSettings,
-//   listItemsSettings,
-// } from './menu';
-// bodySettings();
-// navSettings();
-// mainUlSettings();
-// menuListSettings();
-// dropDownMenuSettings();
-// listItemsSettings();
+import './styles.css';
