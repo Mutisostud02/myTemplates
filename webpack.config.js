d1c363fe60022d6474dc534d5739df62ebbadcd0
Mutisostud02 <mutiso8395@gmail.com>
2024-01-23 23:20:19 +0300

    Added babel loaders and plugin configurations

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,20 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+            cacheDirectory: true
+          }
+        }
+      },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
